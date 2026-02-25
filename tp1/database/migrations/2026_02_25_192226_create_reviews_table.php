4019fdaa36d79cff2849cc7bc4d0dbf5637f6ae1
Tasnime <tr.tasnime@gmail.com>
2026-02-25 14:26:14 -0500

    Création table Reviews

---
+++ tp1/database/migrations/2026_02_25_192226_create_reviews_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->tinyInteger('rating');
+            $table->text('comment');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('rental_id')->references('id')->on('rentals');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
