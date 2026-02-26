4289d858f93b90cb17e12085ce42ef3131dcaefb
Tasnime <tr.tasnime@gmail.com>
2026-02-26 08:29:01 -0500

    Création des fabriques

--- tp1/app/Models/Reviews.php
+++ tp1/app/Models/Reviews.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Reviews extends Model
 {
+    use HasFactory;
 
     public function rental()
     {
---
+++ tp1/database/factories/RentalFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class RentalFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'startDate' => fake()->date(),
+            'endDate' => fake()->date(),
+            'totalPrice' => fake()->randomFloat(2, 5, 500),
+        ];
+    }
+}
