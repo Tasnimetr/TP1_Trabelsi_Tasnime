eed674bd42a236650f581da09192b874597f558c
Tasnime <tr.tasnime@gmail.com>
2026-03-02 06:44:47 -0500

    Méthodes contrôleur equipment

--- tp1/app/Http/Controllers/EquipmentController.php
+++ tp1/app/Http/Controllers/EquipmentController.php
@@ -2,10 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\EquipmentResource;
 use Illuminate\Http\Request;
 use App\Models\Equipment;
+use Exception;
+use Illuminate\Database\QueryException;
 
 class EquipmentController extends Controller
 {
-    //
+    public function index()
+    {
+        try
+        {
+            return EquipmentResource::collection(Equipment::all())->response()->setStatusCode(200);
+        }
+        catch (Exception $e)
+        {
+            abort(500, 'Server error');
+        }
+    }
+
+    public function show($id)
+    {
+        try
+        {
+            return (new EquipmentResource(Equipment::findOrFail($id)))->response()->setStatusCode(200);
+        }
+        catch (QueryException $ex)
+        {
+            abort(404, "Invalid Id");
+        }
+        catch (Exception $ex)
+        {
+            abort(500, "server error");
+        }
+    }
 }
