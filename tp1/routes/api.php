d14d47932df627d4b3524e4bd820add3da029ab4
Tasnime <tr.tasnime@gmail.com>
2026-03-02 14:03:47 -0500

    Remise finale Partie 2

--- tp1/routes/api.php
+++ tp1/routes/api.php
@@ -6,23 +6,23 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-//1 Recevoir l’information de tous les équipements
+//1. Recevoir l’information de tous les équipements
 Route::get('equipment', [EquipmentController::class, 'index']);
 
-//2 Recevoir l’information d’un équipement en particulier
+//2. Recevoir l’information d’un équipement en particulier
 Route::get('equipment/{id}', [EquipmentController::class, 'show']);
 
-//3 Recevoir l’indice de popularité d’un équipement
+//3. Recevoir l’indice de popularité d’un équipement
 Route::get('equipment/{id}/popularity', [EquipmentController::class, 'popularity']);
 
-//4 Créer un utilisateur
+//4. Créer un utilisateur
 Route::post('users', [UserController::class, 'store']); //ou insert?
 
-//5 Mettre à jour un utilisateur (mise à jour complète et non partielle)
+//5. Mettre à jour un utilisateur (mise à jour complète et non partielle)
 Route::put('users/{id}', [UserController::class, 'update']);
 
-//6 Supprimer une critique
+//6. Supprimer une critique
 Route::delete('reviews/{id}', [ReviewController::class, 'destroy']); //ou delete?
 
-//7 Recevoir la moyenne du prix total de location d’un équipement 
+//7. Recevoir la moyenne du prix total de location d’un équipement 
 Route::get('equipment/{id}/average_price', [EquipmentController::class, 'averagePrice']);
