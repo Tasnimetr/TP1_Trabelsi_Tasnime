fd79c5b10fa6c632ab3c9a7e06a5070b74b3faca
Tasnime <tr.tasnime@gmail.com>
2026-03-02 13:30:05 -0500

    Méthode surpession review
    Source consulté pour comprendre quel code retourner: https://restfulapi.net/http-methods/#delete

--- tp1/app/Http/Controllers/ReviewController.php
+++ tp1/app/Http/Controllers/ReviewController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Review;
 
 class ReviewController extends Controller
 {
-    //
+    public function destroy($id){
+        $review = Review::findOrFail($id);
+        $review->delete();
+        //source consultée: https://restfulapi.net/http-methods/#delete
+        return response()->noContent();
+    }
 }
