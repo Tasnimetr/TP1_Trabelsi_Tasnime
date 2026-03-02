103485f8b48e89eeddc1e4f709d88af2ddfaa0fa
Tasnime <tr.tasnime@gmail.com>
2026-03-02 13:27:14 -0500

    Implémentation validation update user

--- tp1/app/Http/Controllers/UserController.php
+++ tp1/app/Http/Controllers/UserController.php
@@ -5,12 +5,21 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Resources\UserResource;
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
 
 class UserController extends Controller
 {
-    public function store(Request $request)
+    public function store(StoreUserRequest $request)
     {
-        $user = User::create($request->all());
-        return (new UserResource($user))->response()->setStatusCode(201);
+        $user = User::create($request->validated());
+        return (new UserResource($user))->response()->setStatusCode(201); //create fait -> Created
+    }
+
+     public function update(UpdateUserRequest $request, $id)
+    {
+        $user = User::findOrFail($id);
+        $user->update($request->validated());
+        return (new UserResource($user))->response()->setStatusCode(201); //update fait -> OK
     }
 }
