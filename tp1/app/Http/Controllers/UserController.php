d46a23ec3d9a92ffe4e89a66138e0f32864bec9e
Tasnime <tr.tasnime@gmail.com>
2026-03-02 08:03:25 -0500

    Méthode création utilisateurs

--- tp1/app/Http/Controllers/UserController.php
+++ tp1/app/Http/Controllers/UserController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Http\Resources\UserResource;
 
 class UserController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $user = User::create($request->all());
+        return (new UserResource($user))->response()->setStatusCode(201);
+    }
 }
