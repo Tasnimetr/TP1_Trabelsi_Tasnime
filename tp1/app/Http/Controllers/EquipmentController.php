c3ca30daa76fe531bb9c380ae03598294550a2e9
Tasnime <tr.tasnime@gmail.com>
2026-03-02 08:02:56 -0500

    Méthode calcul index popularité

--- tp1/app/Http/Controllers/EquipmentController.php
+++ tp1/app/Http/Controllers/EquipmentController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Resources\EquipmentResource;
 use Illuminate\Http\Request;
 use App\Models\Equipment;
+use App\Models\Rental;
+use App\Models\Review;
 use Exception;
 use Illuminate\Database\QueryException;
 
@@ -37,4 +39,24 @@ class EquipmentController extends Controller
             abort(500, "server error");
         }
     }
-}
+
+    public function popularity($id)
+    {
+        $equipment = Equipment::findOrFail($id);
+
+        $totalNumberLocations = $equipment->rentals()->count();
+
+        $rental = Rental::Where('equipment_id', $id)->first();
+
+        if($rental) {
+            $rentalId = $rental->id;
+        } else {
+            $rentalId = 0;
+        }
+
+        $averageRating = Review::where('rental_id', $rentalId)->avg('rating');
+
+        $popularityIndex = ($totalNumberLocations * 0.6) + ($averageRating * 0.4);
+        echo("L'indice de popularité est de " . $popularityIndex);
+    }
+} 
